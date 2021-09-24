3a5a63a35b7fcbf382a53c5d13157e0e273a1ae1
Gerson Gallo <ggallovalle@gmail.com>
2021-09-24 12:27:32 -0600

    feat(nim): 6kyu - arrayDiff
    Notes:
    - I do not thing the immplementation is efficient enough

--- nim_katas/src/nim_katas/sep24.nim
+++ nim_katas/src/nim_katas/sep24.nim
@@ -1,4 +1,16 @@
+import sequtils
 
 func hello_world*(): string = 
     ## Says `Hello World`
     result = "Hello World"
+
+proc arrayDiff*(a: seq[int], b: seq[int]): seq[int] =
+  ## It should remove all elements in seq `a` which are present in seq `b`.
+  const empty: seq[int] = @[]
+  result = 
+    if len(a) == 0:
+      empty
+    elif len(b) == 0:
+      a
+    else:
+      a.filterIt(not b.contains(it))
--- nim_katas/tests/tsep24.nim
+++ nim_katas/tests/tsep24.nim
@@ -8,3 +8,14 @@ test "can say `Hello World`":
   let actual = hello_world()
   #assert
   check want == actual
+
+suite "Array diff":
+  const empty: seq[int] = @[]
+
+  test "Basic tests":
+    check(arrayDiff(@[1, 2], @[1]) == @[2])
+    check(arrayDiff(@[1, 2, 2], @[1]) == @[2, 2])
+    check(arrayDiff(@[1, 2, 2], @[2]) == @[1])
+    check(arrayDiff(@[1, 2, 2], @[]) == @[1, 2, 2])
+    check(arrayDiff(@[], @[1, 2]) == empty)
+    check(arrayDiff(@[1, 2, 3], @[1, 2]) == @[3])
