1eeef0316eb3df1461f56e4068041523f1eb7302
Gerson Gallo <ggallovalle@gmail.com>
2021-09-24 16:17:34 -0600

    defeated(nim): 6 kyu - prime number with step
    Reason:
    - From the iterator I need to take two at a time or something like that

--- nim_katas/src/nim_katas/sep24.nim
+++ nim_katas/src/nim_katas/sep24.nim
@@ -1,10 +1,10 @@
-import sequtils
+import sequtils, strformat
 
 func hello_world*(): string = 
     ## Says `Hello World`
     result = "Hello World"
 
-proc arrayDiff*(a: seq[int], b: seq[int]): seq[int] =
+func arrayDiff*(a: seq[int], b: seq[int]): seq[int] =
   ## It should remove all elements in seq `a` which are present in seq `b`.
   const empty: seq[int] = @[]
   result = 
@@ -19,10 +19,35 @@ proc arrayDiff*(a: seq[int], b: seq[int]): seq[int] =
 type 
   HoursMinSec = array[0..2, int]
 
-proc calculateLead*(a, b, lead: int): HoursMinSec =
+func calculateLead*(a, b, lead: int): HoursMinSec =
   ## Calculate how much time will it take to `b` to catch `a` if a has `lead`.
   ## - if `b` is greater than `b` return [-1, -1, -1]
   if a > b:
     [ -1, -1, -1 ]
   else:
     [ -1, -1, -1 ]
+
+func isPrime(n: int): bool =
+  for x in 2..<n:
+    if n mod x == 0:
+      return false
+  return true
+  
+
+iterator primes(initial, finish: int): int =
+  for it in initial..finish:
+    if it.isPrime:
+      yield it
+
+
+func primeNumbersStep*(step, initial, finish: int): seq[int] =
+    ## Find the first two prime number between `initial` and `finish` which have
+    ## an `step` between the two.
+    # var prev = 0
+    var previous = 0
+    for prime in primes(initial, finish):
+      if prime - previous == initial:
+        return @[previous, initial]
+      previous = prime
+
+    return @[0, 0]
--- nim_katas/tests/tsep24.nim
+++ nim_katas/tests/tsep24.nim
@@ -2,25 +2,29 @@ import unittest
 import nim_katas/sep24
 
 test "can say `Hello World`":
-  #arrange
-  let want = "Hello World"
-  #act
-  let actual = hello_world()
-  #assert
-  check want == actual
+  check hello_world() == "Hello World"
 
 suite "Array diff":
   const empty: seq[int] = @[]
 
   test "Basic tests":
-    check(arrayDiff(@[1, 2], @[1]) == @[2])
-    check(arrayDiff(@[1, 2, 2], @[1]) == @[2, 2])
-    check(arrayDiff(@[1, 2, 2], @[2]) == @[1])
-    check(arrayDiff(@[1, 2, 2], empty) == @[1, 2, 2])
-    check(arrayDiff(@[], @[1, 2]) == empty)
-    check(arrayDiff(@[1, 2, 3], @[1, 2]) == @[3])
+    check arrayDiff(@[1, 2], @[1]) == @[2]
+    check arrayDiff(@[1, 2, 2], @[1]) == @[2, 2]
+    check arrayDiff(@[1, 2, 2], @[2]) == @[1]
+    check arrayDiff(@[1, 2, 2], empty) == @[1, 2, 2]
+    check arrayDiff(@[], @[1, 2]) == empty
+    check arrayDiff(@[1, 2, 3], @[1, 2]) == @[3]
 
-suite "Tortoise race":
-  test "Basic tests":
-    check(calculateLead(720, 850, 70) == [0, 32, 18])
-    check(calculateLead(820, 81, 550) == [-1, -1, -1])
+# suite "Tortoise race":
+#   test "Basic tests":
+#     skip()
+#     check(calculateLead(720, 850, 70) == [0, 32, 18])
+#     check(calculateLead(820, 81, 550) == [-1, -1, -1])
+
+# suite "Step in prime numbers":
+#   test "Basic tests":
+#     check primeNumbersStep(2,100,110) ==  @[101, 103]
+#     check primeNumbersStep(4,100,110) == @[103, 107]
+#     check primeNumbersStep(6,100,110) == @[101, 107]
+#     check primeNumbersStep(8,300,400) ==  @[359, 367]
+#     check primeNumbersStep(10,300,400) ==  @[307, 317]
