c4f7095a605c75d889d2b3641efb0457ff1118fb
Gerson Gallo <ggallovalle@gmail.com>
2021-09-29 15:47:56 -0600

    feat(ts): genetrix.parts - take
    take :: Int -> Iterable a -> Iterable a

--- js-katas/src/genetrix/parts.spec.ts
+++ js-katas/src/genetrix/parts.spec.ts
@@ -1,22 +1,38 @@
-import { chunked } from "./parts"
+import { chunked, take } from "./parts"
 import { range } from "./range"
 
 describe("genetrix.parts", () => {
-  describe("chunked", () => {
-    test("`size=2` with sut par", () => {
-      const sut = range(3)
-      const res = [...chunked(2, sut)]
-      expect(res.length).toBe(2)
-      expect(res[0]).toEqual([0, 1])
-      expect(res[1]).toEqual([2, 3])
+  describe("take", () => {
+    test("`n=2` returns 2 elements", () => {
+      const source = range(3)
+      const actual = [...take(2, source)]
+      expect(actual.length).toBe(2)
+      expect(actual).toEqual([0, 1])
     })
 
-    test("`size=2` with sut inpar", () => {
-      const sut = range(2)
-      const res = [...chunked(2, sut)]
-      expect(res.length).toBe(2)
-      expect(res[0]).toEqual([0, 1])
-      expect(res[1]).toEqual([2])
+    test("`n` > `source`.length returns all source", () => {
+      const source = range(3)
+      const actual = [...take(1000, source)]
+      expect(actual.length).toBe(4)
+      expect(actual).toEqual([0, 1, 2, 3])
+    })
+  })
+
+  describe("chunked", () => {
+    test("`size=2` and source par", () => {
+      const source = range(3)
+      const actual = [...chunked(2, source)]
+      expect(actual.length).toBe(2)
+      expect(actual[0]).toEqual([0, 1])
+      expect(actual[1]).toEqual([2, 3])
+    })
+
+    test("`size=2` and source inpar", () => {
+      const source = range(2)
+      const actual = [...chunked(2, source)]
+      expect(actual.length).toBe(2)
+      expect(actual[0]).toEqual([0, 1])
+      expect(actual[1]).toEqual([2])
     })
 
     test("`size` > `source`.length", () => {
@@ -24,6 +40,13 @@ describe("genetrix.parts", () => {
       const res = [...chunked(1000, sut)]
       expect(res.length).toBe(1)
       expect(res[0]).toEqual([0, 1, 2])
+    })
+
+    test("throws when `size` <= 0", () => {
+      const sut = range(2)
+      expect(() => {
+        const res = [...chunked(0, sut)]
+      }).toThrow(RangeError)
     })
   })
 })
--- js-katas/src/genetrix/parts.ts
+++ js-katas/src/genetrix/parts.ts
@@ -1,10 +1,21 @@
+export function* take<T>(n: number, source: Iterable<T>): Generator<T> {
+  let counter = 0
+  for (const x of source) {
+    counter++
+    yield x
+    if (counter === n) {
+      break
+    }
+  }
+}
+
 /**
  * Splits the `source` into an array of arrays, each not exiding the given `size`.
  * @param size the number of elements to take in each list.
  * @param source
  */
 export function* chunked<T>(size: number, source: Iterable<T>): Generator<T[]> {
-  if (size <= 0) throw new TypeError("`size` MUST be more than 0")
+  if (size <= 0) throw new RangeError("`size` MUST be >= 0")
   let counter = 0
   let accumulator: T[] = []
   for (const x of source) {
